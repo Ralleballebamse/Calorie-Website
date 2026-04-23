ba82afb2276413d9bb66519f2f74fafeb17be966
Ralleballebamse <raller@hotmail.se>
2026-04-23 17:22:38 +0200

    Noticed in my footer some didn't have title in bold so fixed now.

--- src/frontend/Components/footer.tsx
+++ src/frontend/Components/footer.tsx
@@ -4,15 +4,15 @@ function Footer() {
         <footer className="flex items-center bg-white shadow pl-6 h-auto relative justify-between pr-30 py-10">
             {/* Left side information*/}
             <aside>
-                <h2 className="text-xl text-[#116a2aca] my-2">VitaTrack</h2>
+                <h2 className="text-xl text-[#116a2aca] my-2 font-bold">VitaTrack</h2>
                 <p>Precision weight management for </p>
                 <p>modern era. Your health, decided.</p>
-                <p className="py-5 text-[#116a2aca]">© 2024 VitaTrack Health. Precision Wellness.</p>
+                <p className="py-5 text-[#116a2aca] font-bold">© 2024 VitaTrack Health. Precision Wellness.</p>
             </aside>
 
             {/* Company and nav under */}
             <div className="flex flex-col gap-4">
-                <h3 className="text-[#116a2aca]">Company</h3>
+                <h3 className="text-[#116a2aca] font-bold">Company</h3>
                 <nav className="flex flex-col gap-2 items-start underline">
                     <button>About us</button>
                     <button>Careers</button>
@@ -22,7 +22,7 @@ function Footer() {
 
             {/* Legal and nav under */}
             <div className="flex flex-col gap-4">
-                <h3 className="text-[#116a2aca]">Legal</h3>
+                <h3 className="text-[#116a2aca] font-bold">Legal</h3>
                 <nav className="flex flex-col gap-2 items-start underline">
                     <button>Privacy Policy</button>
                     <button>Terms of Service</button>
@@ -32,7 +32,7 @@ function Footer() {
 
             {/* Support and nav under */}
             <div className="flex flex-col gap-4">
-                <h3 className="text-[#116a2aca]">Support</h3>
+                <h3 className="text-[#116a2aca] font-bold">Support</h3>
                 <nav className="flex flex-col gap-2 items-start underline">
                     <button>Help Center</button>
                     <button>Guides</button>
