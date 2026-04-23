6f3bf008a46f32778620427f29d8dd4d7ca54bc2
Ralleballebamse <raller@hotmail.se>
2026-04-23 02:58:10 +0200

    Created an footer that will be used for my other slides.

---
+++ src/frontend/Components/footer.tsx
@@ -0,0 +1,43 @@
+
+function Footer() {
+    return (
+        <footer className="flex items-center bg-white shadow pl-6 h-auto relative justify-between pr-30">
+            <aside>
+                <h2 className="text-xl text-[#116a2aca] my-2">VitaTrack</h2>
+                <p>Precision weight management for </p>
+                <p>modern era. Your health, decided.</p>
+                <p className="py-5 text-[#116a2aca]">© 2024 VitaTrack Health. Precision Wellness.</p>
+            </aside>
+
+            <div className="flex flex-col gap-4">
+                <h3 className="text-[#116a2aca]">Company</h3>
+                <nav className="flex flex-col gap-2 items-start underline">
+                    <button>About us</button>
+                    <button>Careers</button>
+                    <button>Contact</button>
+                </nav>
+            </div>
+
+            <div className="flex flex-col gap-4">
+                <h3 className="text-[#116a2aca]">Legal</h3>
+                <nav className="flex flex-col gap-2 items-start underline">
+                    <button>Privacy Policy</button>
+                    <button>Terms of Service</button>
+                    <button>Legal</button>
+                </nav>
+            </div>
+
+            <div className="flex flex-col gap-4">
+                <h3 className="text-[#116a2aca]">Support</h3>
+                <nav className="flex flex-col gap-2 items-start underline">
+                    <button>Help Center</button>
+                    <button>Guides</button>
+                    <button>API Docs</button>
+                </nav>
+            </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
