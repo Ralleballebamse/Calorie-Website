6603cfd50429a3144b7379c443c7328a8bb6e1f9
Ralleballebamse <raller@hotmail.se>
2026-04-23 16:55:39 +0200

    Now I have made one of the third containers in homepage.

--- src/frontend/homePage.tsx
+++ src/frontend/homePage.tsx
@@ -1,12 +1,33 @@
-import React from 'react'
 import "./index.css";
+import Header from "./Components/header";
+import Footer from "./Components/footer";
 
 function LoginPage() {
   return (
     <div className="min-h-screen flex flex-col bg-[#f9f5ff]">
-      <main>
-        <h1 className='bg-black'>Hello</h1>
+      <Header />
+      <main className="h-auto py-30">
+        <div className="flex justify-between pb-40 px-10">
+          <div className="flex flex-col gap-10 max-w-1/2">
+            <div className="flex gap-2 bg-[#bfcec0] rounded-xl justify-center items-center w-1/2">
+              <i className="fa-solid fa-shield-heart"></i>
+              <p>Precision Wellness Monitering</p>
+            </div>
+            <h2 className="text-5xl font-bold">Track Your Journey, Transform Your Health</h2>
+            <p>The professional way to monitor your progress and achieve your wellness goals.
+              Experience a clinical-grade tracking interface
+              designed for holistic clarity.
+            </p>
+            <nav className="flex gap-5">
+              <button className="bg-[#1B3022] text-white h-14 w-50 text-2xl rounded-2xl">Get Started</button>
+              <button className="bg-[#bfcec0] text-[#3e5d48] h-14 w-50 text-2xl rounded-2xl">Log In</button>
+            </nav>
+          </div>
+          <img src="Pictures/HeadPicture.png" alt="Statistics about weighlosing" className="rounded-2xl" />
+        </div>
+
       </main>
+      <Footer />
     </div>
   );
 }
