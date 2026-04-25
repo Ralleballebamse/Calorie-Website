06a6a612043d6e601f1b2062f447dba3d20c6492
Ralleballebamse <raller@hotmail.se>
2026-04-25 22:32:15 +0200

    Now fixed the navigation in header, fixed also so the highligthed button is correct.

--- src/frontend/Components/header.tsx
+++ src/frontend/Components/header.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
 
-  const [selected, setSelected] = useState<"Home" | "Feature" | "Dashboard" | "Tracking" | null>("Home");
+  const navigate = useNavigate();
+
+  const isActive = (path: string) => location.pathname === path;
 
   return (
     <header className="flex items-center bg-white shadow px-6 h-18 relative">
@@ -13,29 +15,29 @@ function Header() {
         {/* Center */}
         <nav className="absolute left-1/2 -translate-x-1/2 flex gap-20 text-xl">
           <button
-            onClick={() => setSelected("Home")}
-            className={`${selected == "Home"
+            onClick={() => navigate("/")}
+            className={`${isActive("/")
               ? "!text-[#8BA88E] pb-1 border-b-2 border-[#8BA88E]"
               : ""
               }`}
           >Home</button>
           <button
-            onClick={() => setSelected("Feature")}
-            className={`${selected == "Feature"
+            onClick={() => navigate("/feature")}
+            className={`${isActive("/feature")
               ? "!text-[#8BA88E] pb-1 border-b-2 border-[#8BA88E]"
               : ""
               }`}
           >Features</button>
           <button
-            onClick={() => setSelected("Dashboard")}
-            className={`${selected == "Dashboard"
+            onClick={() => navigate("/dashboard")}
+            className={`${isActive("/dashboard")
               ? "!text-[#8BA88E] pb-1 border-b-2 border-[#8BA88E]"
               : ""
               }`}
           >Dashboard</button>
             <button
-            onClick={() => setSelected("Tracking")}
-            className={`${selected == "Tracking"
+            onClick={() => navigate("/tracking")}
+            className={`${isActive("/tracking")
               ? "!text-[#8BA88E] pb-1 border-b-2 border-[#8BA88E]"
               : ""
               }`}
@@ -44,9 +46,13 @@ function Header() {
 
         {/* Right */}
         <div className="ml-auto flex items-center gap-4 text-xl">
-          <button className="whitespace-nowrap hover:underline text-[#8BA88E]">Log In</button>
+          <button 
+          onClick={() => navigate("/login")}
+          className="whitespace-nowrap hover:underline text-[#8BA88E]">Log In</button>
           <div className="bg-[#1B3022] rounded-2xl px-4 py-1.5 flex justify-center hover:bg-[#3e5d48]">
-            <button className="text-white">Get Started</button>
+            <button 
+            onClick={() => navigate("/create")}
+            className="text-white">Get Started</button>
           </div>
         </div>
     </header>
