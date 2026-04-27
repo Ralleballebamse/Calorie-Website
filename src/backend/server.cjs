b4a488b7da5c48c1610e62fb24426d81f7e20563
Ralleballebamse <raller@hotmail.se>
2026-04-27 16:43:42 +0200

    Ehm somehow I removed by backend from running on port x but now it all works again

--- src/backend/server.cjs
+++ src/backend/server.cjs
@@ -22,3 +22,9 @@ mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error("MongoDB error:", err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
