3c18eedce9ed7ea5c0f9183da5291e1b33e3609b
Ralleballebamse <raller@hotmail.se>
2026-04-27 16:27:33 +0200

    I have now made so no information from env file gets exposed in terminal

--- src/backend/server.cjs
+++ src/backend/server.cjs
@@ -20,11 +20,5 @@ app.use("/api/goals", goalRoutes);
 
 mongoose
     .connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("MongoDB connected");
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on port ${process.env.PORT}`);
-            console.log("Mongo URI:", process.env.MONGO_URI);
-        });
-    })
-    .catch((error) => console.log(error));
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB error:", err));
